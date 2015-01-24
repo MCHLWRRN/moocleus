9cbd7de852eb5f18375954f5f27d2279bbff5d58
MCHLWRRN <me@mchlwrrn.com>
2015-01-24 00:16:02 -0800

    Video uploading with lessons issue

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,30 +1,30 @@
 class Instructor::LessonsController < ApplicationController
-	before_action :authenticate_user!
-	before_action :require_authorized_for_current_section
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
 
-	def new
-		@lesson = Lesson.new
-	end
+  def new
+    @lesson = Lesson.new
+  end
 
-	def create
-		@lesson = current_section.lessons.create(lesson_params)
-		redirect_to instructor_course_path(current_section.course)
-	end
+  def create
+    @lesson = current_section.lessons.create(lesson_params)
+    redirect_to instructor_course_path(current_section.course)
+  end
 
-	private
+  private
 
-	def require_authorized_for_current_section
-		if current_section.course.user != current_user
-			return render :text => 'Unauthorized', :status => :unauthorized
-		end
-	end
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render :text => 'Unauthorized', :status => :unauthorized
+    end
+  end
 
-	helper_method :current_section
-	def current_section
-		@current_section ||= Section.find(params[:section_id])
-	end
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
 
-	def lesson_params
-		params.require(:lesson).permit(:title, :subtitle, :video)
-	end
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle)
+  end
 end
