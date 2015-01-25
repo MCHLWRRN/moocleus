bcbf188882ca53a0222740dd784b804113bb8f38
MCHLWRRN <me@mchlwrrn.com>
2015-01-25 02:50:46 -0800

    Moved new lesson form into a modal

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
   namespace :instructor do
     resources :lessons, :only => [:update]
     resources :sections, :only => [:update] do
-      resources :lessons, :only => [:new, :create]
+      resources :lessons, :only => [:create]
     end
-    resources :courses, :only => [:new, :create, :show]
+    resources :courses, :only => [:new, :create, :show] do
+      resources :sections, :only => [:create]
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
