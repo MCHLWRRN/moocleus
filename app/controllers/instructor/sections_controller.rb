46b46b53f44b08f0f3d12e51d5c951e59250327f
MCHLWRRN <me@mchlwrrn.com>
2015-01-25 00:10:54 -0800

    Added drag and drop feature for sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_course
+	before_action :require_authorized_for_current_course, :only => [:new, :create]
+	before_action :require_authorized_for_current_section, :only => [:update]
 
 	def new
 		@section = Section.new
@@ -11,7 +12,22 @@ class Instructor::SectionsController < ApplicationController
 		redirect_to instructor_course_path(current_course)
 	end
 
+	def update
+		current_section.update_attributes(section_params)
+		render :text => 'updated'
+	end
+
 	private
+
+	def require_authorized_for_current_section
+		if current_section.course.user != current_user
+			render :text => 'Unauthorized', :status => :unauthorized
+		end
+	end
+
+	def current_section
+		@current_section ||= Section.find(params[:id])
+	end
 
 	def require_authorized_for_current_course
 		if current_course.user != current_user
@@ -25,7 +41,7 @@ class Instructor::SectionsController < ApplicationController
 	end
 
 	def section_params
-		params.require(:section).permit(:title)
+		params.require(:section).permit(:title, :row_order_position)
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125013343) do
+ActiveRecord::Schema.define(version: 20150125072054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,9 +56,11 @@ ActiveRecord::Schema.define(version: 20150125013343) do
     t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
   add_index "sections", ["course_id"], name: "index_sections_on_course_id", using: :btree
+  add_index "sections", ["row_order"], name: "index_sections_on_row_order", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
